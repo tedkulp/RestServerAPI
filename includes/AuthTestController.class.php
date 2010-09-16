87efccb305def2eff76b69b3a1b9dcb7005987d2
nuno <nuno@7f1b6534-bc81-4b3a-9ace-5d31f77aa645>
2010-09-16 17:03:26 +0000

    added config root_url to ::verify
    git-svn-id: http://svn.cmsmadesimple.org/svn/restserverapi/trunk@6 7f1b6534-bc81-4b3a-9ace-5d31f77aa645

--- includes/AuthTestController.class.php
+++ includes/AuthTestController.class.php
@@ -10,16 +10,27 @@ class AuthTestController implements RestController {
 
     public function verify(RestServer $rest) {
         global $gCms;
+		$config = $gCms->GetConfig();
         $post = $rest->getRequest()->getPost();
-error_log(print_r($post));
+         error_log(print_r($post));  
 		$db = &$gCms->GetDb();
-		$check = ($db->GetOne('select count(*) from '.cms_db_prefix().'users where username=? and password=?',
-			array($post['username'],md5($post['password']))) > 0);
+		$AuthCheck = $db->GetOne('select count(*) from '.cms_db_prefix().'users where username=? and password=?',
+			array($post['username'],md5($post['password'])));
+		
+		$domain = $config['root_url'];
+		$postedDomain = $post['domain'];
+			
+			if($AuthCheck > 0 && $postedDomain == $domain) {
+			  $check = true;
+			  }
+			  else{
+			  $check = false;
+			  }
 
 		$json = new Services_JSON();
         $rest->getResponse()->addHeader("Content-Type: application/json; charset=utf-8");
         $rest->getResponse()->addHeader("Content-Description: File Transfer");
-		$rest->getResponse()->addHeader("Content-Disposition: attachment; filename=pages.json");
+		$rest->getResponse()->addHeader("Content-Disposition: attachment; filename=authTest.json");
         $rest->getResponse()->setResponse($json->encode($check));
 
         return $rest;
